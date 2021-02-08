f41ab8509a46ec96eb7c5c19924294c59d3dc580
Naveen Chandra P <naveenchander10@gmail.com>
2021-02-08 12:31:29 +0530

    remove duplicate from sorted list added O(1) storage

---
+++ Remove Duplicate from Sorted List/RmvDuplifrmSrtdList.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+
+/**
+ * RmvDuplifrmSrtdList
+ */
+public class RmvDuplifrmSrtdList {
+
+    // Check if the list is sorted or not
+    public boolean isSorted(ArrayList<Integer> list) {
+        int back = 0;
+        int front = 1;
+        // parse through the list
+        while (front < list.size()) {
+            // check if back equal to front
+            if (list.get(back) > list.get(front)) {
+                return false;
+            } else {
+                back++;
+                front++;
+            }
+        }
+        return true;
+    }
+
+    // remove duplicate from sorted list, O(1) space complexity
+    public ArrayList<Integer> rmvDupli(ArrayList<Integer> list) {
+        int back = 0;
+        int front = 1;
+        // parse through the list
+        while (front < list.size()) {
+            // check if back equal to front
+            if (list.get(back) == list.get(front)) {
+                list.remove(back);
+            } else {
+                back++;
+                front++;
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        RmvDuplifrmSrtdList rsl = new RmvDuplifrmSrtdList();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(4);
+        list.add(4);
+        list.add(4);
+        list.add(5);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(9);
+        if (rsl.isSorted(list))
+            System.out.println(rsl.rmvDupli(list));
+        else
+            System.out.println("List is not sorted");
+
+    }
+}
