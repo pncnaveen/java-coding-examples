e1f843a4c187b68329294dbea8b649404fedcb6e
Naveen Chandra P <naveenchander10@gmail.com>
2021-02-17 23:21:05 +0530

    word search implemented

--- word search/word_search.java
+++ word search/word_search.java
@@ -13,51 +13,48 @@ public class word_search {
     }
 
     // backtrack and check if the word is ound or not
-    public boolean backTrack(char[][] matrix, String word, int x, int y, int row, int col, int count, int wordLen) {
-
-        boolean k = false;
-        if (count == wordLen - 1) {
-            System.out.println("checking count");
-            return true;
-        }
-        if (matrixOutOfBounds(x, y, row, col) == true) {
-            System.out.println("checking matrix out of bounds");
+    public boolean backTrack(char[][] matrix, String word, int x, int y, int row, int col, int count) {
+        // check for matrix out of bounds
+        if (matrixOutOfBounds(x, y, row, col)) {
             return false;
         }
-        if (matrix[x][y] == word.charAt(count) && !matrixOutOfBounds(x + 1, y, row, col)
-                && matrix[x + 1][y] == word.charAt(count + 1)) {
-            System.out.println("checking matrix to down side" + x);
-            return backTrack(matrix, word, x + 1, y, row, col, count + 1, wordLen);
+        // last letter of word found
+        if (matrix[x][y] == word.charAt(count) && count == word.length() - 1) {
+            return true;
         }
-        if (matrix[x][y] == word.charAt(count) && !matrixOutOfBounds(x, y + 1, row, col)
-                && matrix[x][y + 1] == word.charAt(count + 1)) {
-            System.out.println("checking matrix to right side" + y);
-            return backTrack(matrix, word, x, y + 1, row, col, count + 1, wordLen);
+        // if letter is found
+        if (matrix[x][y] == word.charAt(count)) {
+            // check right of matrix
+            if (backTrack(matrix, word, x + 1, y, row, col, count + 1)) {
+                return true;
+            }
+            // check left of matrix
+            if (backTrack(matrix, word, x, y + 1, row, col, count + 1)) {
+                return true;
+            }
         }
-        System.out.println("no check going down" + x);
-        k = backTrack(matrix, word, x + 1, y, row, col, count, wordLen);
-        // k = backTrack(matrix, word, x, y + 1, row, col, count, wordLen);
-        // return k;
-        return k;
+        count = 0;
+        if (backTrack(matrix, word, x + 1, y, row, col, count)) {
+            return true;
+        } else if (backTrack(matrix, word, x, y + 1, row, col, count)) {
+            return true;
+        }
+        return false;
     }
 
     // search the word in the matrix
     // get 3 params 1: matrix, 2: word, 3: num of row
     // if found return true else return false
-    public boolean searchWord(char[][] matrix, String word, int row, int wordLen) {
-
-        return backTrack(matrix, word, 0, 0, row, row, 0, wordLen);
+    public boolean searchWord(char[][] matrix, String word, int row) {
+        return backTrack(matrix, word, 0, 0, row, row, 0);
     }
 
     public static void main(String[] args) {
         word_search ws = new word_search();
-        char[][] matrix = { { 'B', 'X', 'A', 'M' }, { 'A', 'F', 'A', 'M' }, { 'F', 'O', 'A', 'M' },
+        char[][] matrix = { { 'F', 'A', 'C', 'I' }, { 'O', 'B', 'Q', 'P' }, { 'A', 'N', 'O', 'B' },
                 { 'M', 'A', 'S', 'S' }, };
-        String word = "FOAM";
+        String word = "BP";
         int row = matrix.length;
-
-        boolean isWordFound = ws.searchWord(matrix, word, row - 1, word.length());
-        System.out.println(isWordFound);
+        System.out.println(ws.searchWord(matrix, word, row - 1));
     }
-
 }
